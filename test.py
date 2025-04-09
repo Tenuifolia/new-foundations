aa70363ed33bfddad1aeec7d1ab3327d9799e5a4
Michael Alder <malder@devpipeline.com>
2025-04-09 14:17:44 -0600

    Saved test.py

---
+++ test.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+connection = sqlite3.connect("competency_tracking.db")
+cursor = connection.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS users (
+    user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    phone TEXT,
+    email TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    active BOOLEAN DEFAULT 1,
+    date_created DATETIME DEFAULT CURRENT_TIMESTAMP,
+    hire_date DATE,
+    user_type TEXT CHECK(user_type IN ('user', 'manager')) NOT NULL
+);
+""")
